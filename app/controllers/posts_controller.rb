b61d221d290d49a13fbf9d6cf3769ca75731fd96
Diogo Caetano <diogo.caetano.alves@gmail.com>
2016-02-23 22:30:02 +0000

    ajustando layout arquivos do deviser

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  layout "admin"
   before_filter :authenticate_user!
 
   # GET /posts
