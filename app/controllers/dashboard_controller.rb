113378f79be6040beb59fab326d31933aff57b4e
Diogo Caetano <diogo.caetano.alves@gmail.com>
2016-03-23 14:18:00 +0000

    validacao de usuario existente

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   
   #github teste = julianalucena
-  
+
   def index
 
     @user = User.where(login: params[:github_user]).first
@@ -10,42 +10,46 @@ class DashboardController < ApplicationController
       
       @user = github_user = ApiConnect.get_user(params[:github_user])
 
-      @repositories = user_repositories = ApiConnect.get_user_repositories(github_user.login)
+      if not @user.nil?
 
-      user = User.create(
-        login: github_user.login, 
-        avatar_url: github_user.avatar_url, 
-        url: github_user.url,
-        html_url: github_user.html_url,
-        repos_url: github_user.repos_url,
-        type: github_user.type,
-        public_repos: github_user.public_repos,
-        company: github_user.company,
-        following: github_user.following,
-        location: github_user.location
-      )
+        @repositories = user_repositories = ApiConnect.get_user_repositories(github_user.login)
 
-      user_repositories.each do |repository|
-      
-          Repository.create(
-            user_id: user.id, 
-            name: repository.name, 
-            full_name: repository.full_name,
-            description: repository.description,
-            private: repository.private,
-            fork: repository.fork,
-            url: repository.url,
-            html_url: repository.html_url,
-            archive_url: repository.archive_url,
-            assignees_url: repository.assignees_url,
-            collaborators_url: repository.collaborators_url,
-            commits_url: repository.commits_url,
-            contributors_url: repository.contributors_url,
-            forks_url: repository.forks_url,
-            created_at: repository.created_at
-          )
+        user = User.create(
+          login: github_user.login, 
+          avatar_url: github_user.avatar_url, 
+          url: github_user.url,
+          html_url: github_user.html_url,
+          repos_url: github_user.repos_url,
+          type: github_user.type,
+          public_repos: github_user.public_repos,
+          company: github_user.company,
+          following: github_user.following,
+          location: github_user.location
+        )
+          
+        user_repositories.each do |repository|
+        
+            Repository.create(
+              user_id: user.id, 
+              name: repository.name, 
+              full_name: repository.full_name,
+              description: repository.description,
+              private: repository.private,
+              fork: repository.fork,
+              url: repository.url,
+              html_url: repository.html_url,
+              archive_url: repository.archive_url,
+              assignees_url: repository.assignees_url,
+              collaborators_url: repository.collaborators_url,
+              commits_url: repository.commits_url,
+              contributors_url: repository.contributors_url,
+              forks_url: repository.forks_url,
+              created_at: repository.created_at
+            )
 
-      end  
+        end  
+
+      end
 
     else    
       
--- app/models/api_connect.rb
+++ app/models/api_connect.rb
@@ -2,12 +2,17 @@
 class ApiConnect
 
 	def self.get_user(user_name)
-		github_user =  Hashie::Mash.new HTTParty.get("https://api.github.com/users/#{user_name}").parsed_response		
+		github_user =  Hashie::Mash.new HTTParty.get("https://api.github.com/users/#{user_name}").parsed_response	
+		
+		if  github_user.message.present? && github_user.message.to_s == "Not Found"
+			github_user = nil
+		end
+		
+		github_user
 	end
 
 	def self.get_user_repositories(user_login)
 		user_repositories_hash = HTTParty.get("https://api.github.com/users/#{user_login}/repos").parsed_response
 		user_repositories = user_repositories_hash.map { |hash| Hashie::Mash.new(hash) }	
-		
 	end
 end
