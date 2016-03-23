02d098dd131fb5ca0fcb3969bd7ab6ac508885f3
Diogo Caetano <diogo.caetano.alves@gmail.com>
2016-03-23 11:50:12 +0000

    ajustes na API do GITHUB e na logica principal do projeto

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,14 +4,52 @@ class DashboardController < ApplicationController
     @user = User.where(login: params[:github_user]).first
     
     if not @user
-      @user = ApiConnect.get_user(params[:github_user])
-      ApiConnect.get_user_repositories(@user)
+      @user = github_user = ApiConnect.get_user(params[:github_user])
+
+        p github_user.login
+        p '*****************************************************'
+               
+      @repositories = user_repositories = ApiConnect.get_user_repositories(github_user.login)
+
+      user = User.create(
+        login: github_user.login, 
+        avatar_url: github_user.avatar_url, 
+        url: github_user.url,
+        html_url: github_user.html_url,
+        repos_url: github_user.repos_url,
+        type: github_user.type
+      )
+
+      user_repositories.each do |repository|
+      
+          Repository.create(
+            user_id: user.id, 
+            name: repository.name, 
+            full_name: repository.full_name,
+            description: repository.description,
+            private: repository.private,
+            fork: repository.fork,
+            url: repository.url,
+            html_url: repository.html_url,
+            archive_url: repository.archive_url,
+            assignees_url: repository.assignees_url,
+            collaborators_url: repository.collaborators_url,
+            commits_url: repository.commits_url,
+            contributors_url: repository.contributors_url,
+            forks_url: repository.forks_url,
+            created_at: repository.created_at
+          )
+
+      end  
+
     else
-      if @user.repositories.empty?
-        @repositories = ApiConnect.get_user_repositories(@user)
-      else
-        @repositories = Repository.where(user_id: @user.id).order('name','created_at')
-      end
+     
+      
+        @repositories = ApiConnect.get_user_repositories(@user.login)
+      # else
+        # @repositories = Repository.where(user_id: @user.id).order('name','created_at')
+      # end
     end
   end
+
 end
--- app/models/api_connect.rb
+++ app/models/api_connect.rb
@@ -2,46 +2,12 @@
 class ApiConnect
 
 	def self.get_user(user_name)
-		github_user = HTTParty.get("https://api.github.com/users/#{user_name}").parsed_response
-		
-		user = User.create(
-			login: github_user['login'], 
-			avatar_url: github_user['avatar_url'], 
-			url: github_user['url'],
-			html_url: github_user['html_url'],
-			repos_url: github_user['repos_url'],
-			type: github_user['type']
-		)
-
-		user
+		github_user =  Hashie::Mash.new HTTParty.get("https://api.github.com/users/#{user_name}").parsed_response		
 	end
 
-	def self.get_user_repositories(user)
-		user_repositories = HTTParty.get("https://api.github.com/users/#{user.login}/repos").parsed_response
+	def self.get_user_repositories(user_login)
+		user_repositories_hash = HTTParty.get("https://api.github.com/users/#{user_login}/repos").parsed_response
+		user_repositories = user_repositories_hash.map { |hash| Hashie::Mash.new(hash) }	
 		
-		user_repositories.each do |repository|
-			
-			saved_repositories = Repository.where(user_id: user.id, name: repository['name'])
-
-			if saved_repositories.empty?
-				Repository.create(
-					user_id: user.id, 
-					name: repository['name'], 
-					full_name: repository['full_name'],
-					description: repository['description'],
-					private: repository['private'],
-					fork: repository['fork'],
-					url: repository['url'],
-					html_url: repository['html_url'],
-					archive_url: repository['archive_url'],
-					assignees_url: repository['assignees_url'],
-					collaborators_url: repository['collaborators_url'],
-					commits_url: repository['commits_url'],
-					contributors_url: repository['contributors_url'],
-					forks_url: repository['forks_url'],
-					created_at: repository['created_at']
-				)
-			end
-		end 
 	end
 end
